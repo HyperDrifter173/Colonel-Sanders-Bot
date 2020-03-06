1d8887821158a99f11a9058073dea3ba7ef1a9e5
HyperDrifter173 <56806708+HyperDrifter173@users.noreply.github.com>
2020-03-06 15:27:25 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-
+ 
+const token = 'NjM1MTg0MzU5NDk3NzkzNTU5.XmLX_g.iZbSOqVxuF-Bx5yC4sxu3FJU3eI';
+ 
 bot.on('ready', ()=>{
     console.log('This bot is online');
     bot.user.setActivity('I Love You, Colonel Sanders! A Finger Lickin’ Good Dating Simulator');
 });
-
+ 
 var sanders;
-
+ 
 bot.on('message', msg=>{
     if (msg.content.includes ("hungry")) {
         sanders = Math.floor(Math.random()*4);
@@ -20,7 +22,7 @@ bot.on('message', msg=>{
         };
             
         if (sanders === 1){
-            msg.channel.send('If you need something to fill you up than try my Mac & Cheese $5 Fill Up from your local KFC https://www.kfc.com/ ');
+            msg.channel.send('If you need something to fill you up then try my Mac & Cheese $5 Fill Up from your local KFC https://www.kfc.com/ ');
         
         };
         
@@ -33,7 +35,7 @@ bot.on('message', msg=>{
     sanders = null
     //This should break any potential for a rapid fire spam loop. Place it at the end of wherever you use RNG.
 });
-
+ 
 bot.on('message' , msg=>{
     if (msg.content.includes ("failure")){
         sanders = Math.floor(Math.random()*2);
@@ -46,11 +48,11 @@ bot.on('message' , msg=>{
         };
     };
 });
-
+ 
 bot.on('message' , msg=>{
     if (msg.content.includes ("chicken")){
         sanders = Math.floor(Math.random()*2);
-
+ 
         if (sanders == 0){
         msg.channel.send('My, my I hope you’re talking about my world famous Original Recipe® Chicken! Now located at your local KFC');                
     };
@@ -59,18 +61,18 @@ bot.on('message' , msg=>{
     };
   };
 });
-
+ 
 bot.on('message' , msg=>{
     if (msg.content.includes("disgusting")){
         sanders = Math.floor(Math.random()*2);
-
+ 
         if(sanders === 0){
         msg.channel.send('You better not be talking shit about my Kentucky Fried Chicken or else I’ll Kentucky Fry you.');
     };
     if(sanders === 1){
-        msg.channel.send('The only thing here Disgusting are Furries unless one of you is a Chicken Furry which in that case your the perfect advertisment tool and I love advertisment!')
+        msg.channel.send('The only thing here Disgusting are Furries unless one of you is a Chicken Furry which in that case youre the perfect advertisement tool and I love advertisement!')
     };
 };
 });
-
-bot.login(process.env.BOT_TOKEN);
+ 
+bot.login(token);
